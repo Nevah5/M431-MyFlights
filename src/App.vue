c5268b47de2e67ab890f87c97b5853c287b9006d
Nevah5 <neevaah5@gmail.com>
2022-06-09 16:32:33 +0200

    #4 Finished Footer

--- src/App.vue
+++ src/App.vue
@@ -16,3 +16,28 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Cairo&family=Secular+One&display=swap");
+
+h1.logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &::before {
+    content: "";
+    display: inline-block;
+    background-image: url("@/assets/logo.png");
+    background-position: center;
+    background-size: 100%;
+    height: 50px;
+    aspect-ratio: 1 / 1;
+  }
+  &::after {
+    content: "MyFlights";
+    font-family: "Secular One", sans-serif;
+    color: #088fd5;
+  }
+}
+</style>
