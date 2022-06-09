7e39bf551962a0ad0b31a8cbe54b177a26a17b21
Jonas Herth <jonasherth@gmx.ch>
2022-06-09 16:34:57 +0200

    #3 Added TopNavbar Component to App

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <TopNavbar />
+  <router-view />
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import TopNavbar from "./components/TopNavbar.vue";
+export default {
+  name: "App",
+  components: {
+    TopNavbar,
+  },
+};
+</script>
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Cairo&display=swap");
 </style>
