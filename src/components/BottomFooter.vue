14d372023a3c8af4405a1d8e47076621f99529c3
Nevah5 <neevaah5@gmail.com>
2022-06-09 16:00:31 +0200

    #4 Added bottom line in Footer

---
+++ src/components/BottomFooter.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer>
+    <div class="line">
+      <span>&copy; 2022</span>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "BottomFooter",
+};
+</script>
+
+<style lang="scss" scoped>
+footer {
+  div.line {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    background-color: #144a8f;
+    height: 4px;
+    border-radius: 0.3em;
+    span {
+      padding: 0 3px;
+      background-color: white;
+    }
+  }
+}
+</style>
