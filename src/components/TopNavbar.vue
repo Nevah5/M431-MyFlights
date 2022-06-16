94549abb043280307ed0f0136c09b2ff3ae8cd0a
Jonas Herth <jonasherth@gmx.ch>
2022-06-16 14:25:43 +0200

    #3 WIP TopNavbar components

---
+++ src/components/TopNavbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav>
+    <text>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/contact">Contact</router-link>
+    </text>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: " TopNavbar",
+};
+</script>
+
+<style lang="scss" scoped>
+nav {
+  font-family: "Cairo", sans-serif;
+  background-color: white;
+  position: fixed;
+  box-shadow: 5px 3px 8px #888888;
+  top: 0;
+  width: 100%;
+
+  text {
+    margin-left: 86%;
+    font-size: 20px;
+  }
+
+  a {
+    text-decoration: none;
+    font-family: "Cairo", sans-serif;
+  }
+}
+</style>
