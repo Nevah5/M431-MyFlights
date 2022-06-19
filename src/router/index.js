8ab28ad7569c25b7e16723c4f4437c63cb6aac96
Jonas Herth <jonasherth@gmx.ch>
2022-06-19 16:41:07 +0200

    404 added

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: () => import('@/views/HomeView.vue')
+    path: "/",
+    name: "home",
+    component: () => import("@/views/HomeView.vue"),
   },
   {
-    path: '/about',
-    name: 'about',
-    component: () => import('@/views/AboutView.vue')
-  }
-]
+    path: "/about",
+    name: "about",
+    component: () => import("@/views/AboutView.vue"),
+  },
+  {
+    path: "/:catchAll(.*)",
+    name: "notFound",
+    component: () => import("@/views/NotFoundView.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="NotFound">
+     <img class="NotFoundPic" alt="logo" src="../assets/logo.png">
+    <h2>404 Page not Found</h2>
+    <h2>:(</h2>
+  </div>
+</template>
+
+<style>
+h2 {
+  font-family: sans-serif;
+  font-size: 250%;
+}
+
+.NotFoundPic{
+  margin-left: 30%;
+  margin-top:15%;
+  width: 200px;
+  height:200px;
+}
+</style>
