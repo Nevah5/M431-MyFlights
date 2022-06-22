da93a8c40bb4b4442b036b26854c471b020637be
Nevah5 <neevaah5@gmail.com>
2022-06-22 16:24:16 +0200

    Updated not found page styling

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,21 +1,45 @@
 <template>
   <div class="NotFound">
-     <img class="NotFoundPic" alt="logo" src="../assets/logo.png">
-    <h2>404 Page not Found</h2>
-    <h2>:(</h2>
+    <div class="logo"></div>
+    <caption>
+      <h2>404</h2>
+      <h2>Not Found</h2>
+      <h2>:(</h2>
+    </caption>
   </div>
 </template>
 
-<style>
-h2 {
-  font-family: sans-serif;
-  font-size: 250%;
-}
+<style lang="scss">
+.NotFound {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 180px 0;
 
-.NotFoundPic{
-  margin-left: 30%;
-  margin-top:15%;
-  width: 200px;
-  height:200px;
+  caption {
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    flex-direction: column;
+
+    h2 {
+      margin: 0 30px;
+      font-family: "Secular One", sans-serif;
+      font-size: 250%;
+      color: #088fd6;
+
+      &:last-child {
+        transform: rotate(90deg);
+      }
+    }
+  }
+  .logo {
+    width: 150px;
+    aspect-ratio: 1 / 1;
+    background-image: url("@/assets/logo.png");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
 }
 </style>
