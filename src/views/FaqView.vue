d63aec0289b0ad31c70a622b7a032047be46cd43
Nevah5 <neevaah5@gmail.com>
2022-06-23 16:53:19 +0200

    #12 Added WIP FAQ View

---
+++ src/views/FaqView.vue
@@ -0,0 +1,49 @@
+<template>
+  <h1>FAQ - Frequently Asked Questions</h1>
+  <details v-for="q in questions" :key="q">
+    <summary>{{ q.q }}</summary>
+    <p>{{ q.a }}</p>
+  </details>
+</template>
+
+<script>
+export default {
+  name: "FaqView",
+  data() {
+    return {
+      questions: [
+        { q: "Question1", a: "Answer1" },
+        { q: "Question2", a: "Answer2" },
+        { q: "Question3", a: "Answer3" },
+        { q: "Question4", a: "Answer4" },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  font-family: "Secular One", sans-serif;
+  color: #088fd5;
+}
+details {
+  border-top: solid 3px #088fd5;
+  border-bottom: solid 3px #088fd5;
+  padding: 20px 0;
+  margin: 10px 0;
+
+  summary {
+    font-size: 1.5rem;
+    padding: 0 3vw;
+    font-family: "Secular One", sans-serif;
+    color: #088fd5;
+    cursor: pointer;
+  }
+  *:not(summary) {
+    font-size: 1.3rem;
+    color: #088fd5;
+    padding: 0 3vw;
+  }
+}
+</style>
