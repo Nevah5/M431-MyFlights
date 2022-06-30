80937cef5917d108fcf757de32ba3984686a3381
Nevah5 <neevaah5@gmail.com>
2022-06-30 11:19:19 +0200

    #7 Implemented Fetching with submit

--- src/components/FindFlight.vue
+++ src/components/FindFlight.vue
@@ -1,10 +1,9 @@
 <template>
-  <button onclick="() => flightData(departures, arrival)">Hello</button>
   <section>
     <div class="banner" id="findflight"></div>
     <div class="userinput">
       <h3>From where do you want to depart?</h3>
-      <input list="departures" />
+      <input list="departures" ref="from" />
       <datalist id="departures">
         <option
           v-for="airport in airports"
@@ -15,7 +14,7 @@
         </option>
       </datalist>
       <h3>Where do you want to end up?</h3>
-      <input list="arrivals" />
+      <input list="arrivals" ref="to" />
       <datalist id="arrivals">
         <option
           v-for="airport in airports"
@@ -25,6 +24,7 @@
           {{ getAirportOption(airport) }}
         </option>
       </datalist>
+      <input type="submit" value="Search" :onclick="() => search()" />
     </div>
   </section>
 </template>
@@ -39,12 +39,25 @@ export default {
     };
   },
   methods: {
+    search() {
+      const from = this.$refs.from.value.split(" - ")[0];
+      const to = this.$refs.to.value.split(" - ")[0];
+
+      if (from === "" || to === "") return;
+
+      this.flightData(from, to);
+    },
+    getAirportOption(data) {
+      if (data.name === null) return data.iata;
+      return data.iata + " - " + data.name;
+    },
     flightData(departures, arrival) {
       fetch(
         `https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/cheap?origin=${departures}&page=None&currency=CHF&destination=-${arrival}`,
         {
           method: "GET",
           headers: {
+            "X-RapidAPI-Key": "c51d0a238003f2cce89f10f33daf8c5e",
             "X-Access-Token": "c51d0a238003f2cce89f10f33daf8c5e",
             "X-RapidAPI-Host":
               "travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com",
@@ -105,6 +118,15 @@ section {
       &:focus {
         font-weight: 1000;
         outline: #088fd6 solid 1px;
+      }
+
+      &[type="submit"] {
+        background-color: #088fd6;
+        color: white;
+        font-family: "Secular One", sans-serif;
+        font-size: 1.4em;
+        margin-top: 30px;
+        cursor: pointer;
       }
     }
   }
